caf2e9363f4a32d36382ba449adc48098011bc76
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-08-16 20:11:38 -0300

    :clipboard: impl: Properly using the Next Themes provider to render the dark theme automaticaly

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import NextDarkThemeProvider from "@/components/ui-providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
         "min-h-screen bg-background font-sans antialiased",
         inter.className,
       )}>
-        {children}
+        <NextDarkThemeProvider>
+          {children}
+        </NextDarkThemeProvider>
       </body>
     </html>
   );
